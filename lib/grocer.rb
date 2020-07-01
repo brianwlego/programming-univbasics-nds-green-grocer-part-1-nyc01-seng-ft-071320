67a1206171b82125a3401161a2e6a7bd1f77114b
Brian Lego <legobw@gmail.com>
2020-07-01 21:13:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,10 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  binding.pry
+  collection.each do |key, value|
+    
+    binding.pry
+  end  
 end
 
 def consolidate_cart(cart)

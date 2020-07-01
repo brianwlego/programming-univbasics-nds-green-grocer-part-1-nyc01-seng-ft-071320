cea8d248a43e7fdbd42b4fe78559c1f89d531c40
Brian Lego <legobw@gmail.com>
2020-07-01 21:17:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,6 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |key, value|
     if key[:item] == name
       return_hash = key
+    else return_hash = nil
     end  
   end 
   return_hash
